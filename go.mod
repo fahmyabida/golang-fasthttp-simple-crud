18c8d863f4bb26e1930b3c41bcedb8dc8910134c
fahmyabida <fahmyabida@gmail.com>
2022-07-13 21:01:28 +0700

    change to echo

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-pg/pg/v10 v10.10.1
 	github.com/jeanphorn/log4go v0.0.0-20190526082429-7dbb8deb9468
 	github.com/joho/godotenv v1.3.0
+	github.com/labstack/echo/v4 v4.7.2
 	github.com/pkg/errors v0.9.1
 	github.com/toolkits/file v0.0.0-20160325033739-a5b3c5147e07 // indirect
 	github.com/valyala/fasthttp v1.37.0
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
 func main() {
-	RunApplication()
+	// RunApplication()
+	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{"*"},
+	}))
+	e.GET("/", func(ctx echo.Context) error {
+		return ctx.JSON(200, map[string]interface{}{"msg": "hello"})
+	})
+	e.GET("/hello", func(ctx echo.Context) error {
+		return ctx.JSON(200, map[string]interface{}{"msg": "hello"})
+	})
+	e.Start(":8080")
 }
